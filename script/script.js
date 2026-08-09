5f4aabb2e3ddc509072fc25f5cacd19e2d20e720
Leleandrobs-Lehtech <lehtecell@gmail.com>
2026-08-08 22:58:01 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -33,300 +33,316 @@ var preto3 = 'Mib Homens de Preto';
 var manji3 = 'Jumanji';
 var tix3 = 'Matrix';
 
-
 function soma(){
 
-    var pontos = parseInt(document.getElementById('Placar').value);
+```
+var pontos = parseInt(document.getElementById('Placar').value);
 
-    document.getElementById('Placar').value = pontos + 1;
+document.getElementById('Placar').value = pontos + 1;
 
-    // Quando encontrar os 10 filmes
-    if (pontos == 9){
+// Quando encontrar os 10 filmes
+if (pontos == 9){
 
-        var total = pontos + 1;
+    var total = pontos + 1;
 
-        document.getElementById('msgf').innerHTML =
-            'Parabéns! Você encontrou ' + total + ' filmes!';
+    document.getElementById('msgf').innerHTML =
+        'Parabéns! Você encontrou ' + total + ' filmes!';
 
-        document.getElementById('pontof').style.display = 'flex';
+    document.getElementById('pontof').style.display = 'flex';
 
-        // Define a página de compartilhamento dos 10 pontos
-        document.getElementById('muda').href =
-            'https://leleandrobs-lehtech.github.io/Ca-a-Referencias-Filmes/10pt.html';
-    }
+    // Define a página de compartilhamento dos 10 pontos
+    document.getElementById('muda').href =
+        'https://leleandrobs-lehtech.github.io/Ca-a-Referencias-Filmes/10pt.html';
 }
+```
 
+}
 
 function resposta(){
 
-    filme = document.getElementById("resp").value.trim().toLowerCase();
+```
+filme = document.getElementById("resp").value.trim().toLowerCase();
 
-    document.getElementById("resp").value = "";
+document.getElementById("resp").value = "";
 
 
-    if (
-        filme != nemo && filme != maskara && filme != planeta &&
-        filme != senhor && filme != caribe &&
-        filme != ordinario && filme != vorazes && filme != preto &&
-        filme != manji && filme != tix &&
+if (
+    filme != nemo && filme != maskara && filme != planeta &&
+    filme != senhor && filme != caribe &&
+    filme != ordinario && filme != vorazes && filme != preto &&
+    filme != manji && filme != tix &&
 
-        filme != nemo2 && filme != maskara2 && filme != planeta2 &&
-        filme != senhor2 && filme != caribe2 &&
-        filme != ordinario2 && filme != vorazes2 && filme != preto2 &&
-        filme != manji2 && filme != tix2 &&
+    filme != nemo2 && filme != maskara2 && filme != planeta2 &&
+    filme != senhor2 && filme != caribe2 &&
+    filme != ordinario2 && filme != vorazes2 && filme != preto2 &&
+    filme != manji2 && filme != tix2 &&
 
-        filme != nemo3 && filme != maskara3 && filme != planeta3 &&
-        filme != senhor3 && filme != caribe3 &&
-        filme != ordinario3 && filme != vorazes3 && filme != preto3 &&
-        filme != manji3 && filme != tix3
-    ){
+    filme != nemo3 && filme != maskara3 && filme != planeta3 &&
+    filme != senhor3 && filme != caribe3 &&
+    filme != ordinario3 && filme != vorazes3 && filme != preto3 &&
+    filme != manji3 && filme != tix3
+){
 
-        document.getElementById('alert').style.display = 'flex';
+    document.getElementById('alert').style.display = 'flex';
 
-    }
-
-    else if (filme == nemo || filme == nemo2 || filme == nemo3){
-
-        document.getElementById('nemo').style.display = 'block';
-
-        soma();
-
-        nemo = nemo2 = nemo3 = '0';
-
-    }
-
-    else if (filme == maskara || filme == maskara2 || filme == maskara3){
-
-        document.getElementById('mascara').style.display = 'block';
-
-        soma();
-
-        maskara = maskara2 = maskara3 = '0';
-
-    }
-
-    else if (filme == planeta || filme == planeta2 || filme == planeta3){
-
-        document.getElementById('macaco').style.display = 'block';
-
-        soma();
-
-        planeta = planeta2 = planeta3 = '0';
-
-    }
-
-    else if (filme == senhor || filme == senhor2 || filme == senhor3){
-
-        document.getElementById('anel').style.display = 'block';
-
-        soma();
-
-        senhor = senhor2 = senhor3 = '0';
-
-    }
-
-    else if (filme == caribe || filme == caribe2 || filme == caribe3){
-
-        document.getElementById('pirata').style.display = 'block';
-
-        soma();
-
-        caribe = caribe2 = caribe3 = '0';
-
-    }
-
-    else if (filme == ordinario || filme == ordinario2 || filme == ordinario3){
-
-        document.getElementById('extra').style.display = 'block';
-
-        soma();
-
-        ordinario = ordinario2 = ordinario3 = '0';
-
-    }
-
-    else if (filme == vorazes || filme == vorazes2 || filme == vorazes3){
-
-        document.getElementById('jogos').style.display = 'block';
-
-        soma();
-
-        vorazes = vorazes2 = vorazes3 = '0';
-
-    }
-
-    else if (filme == preto || filme == preto2 || filme == preto3){
-
-        document.getElementById('mib').style.display = 'block';
-
-        soma();
-
-        preto = preto2 = preto3 = '0';
-
-    }
-
-    else if (filme == manji || filme == manji2 || filme == manji3){
-
-        document.getElementById('jumanji').style.display = 'block';
-
-        soma();
-
-        manji = manji2 = manji3 = '0';
-
-    }
-
-    else if (filme == tix || filme == tix2 || filme == tix3){
-
-        document.getElementById('matrix').style.display = 'block';
-
-        soma();
-
-        tix = tix2 = tix3 = '0';
-    }
 }
 
+else if (filme == nemo || filme == nemo2 || filme == nemo3){
 
+    document.getElementById('nemo').style.display = 'block';
+
+    soma();
+
+    nemo = nemo2 = nemo3 = '0';
+
+}
+
+else if (filme == maskara || filme == maskara2 || filme == maskara3){
+
+    document.getElementById('mascara').style.display = 'block';
+
+    soma();
+
+    maskara = maskara2 = maskara3 = '0';
+
+}
+
+else if (filme == planeta || filme == planeta2 || filme == planeta3){
+
+    document.getElementById('macaco').style.display = 'block';
+
+    soma();
+
+    planeta = planeta2 = planeta3 = '0';
+
+}
+
+else if (filme == senhor || filme == senhor2 || filme == senhor3){
+
+    document.getElementById('anel').style.display = 'block';
+
+    soma();
+
+    senhor = senhor2 = senhor3 = '0';
+
+}
+
+else if (filme == caribe || filme == caribe2 || filme == caribe3){
+
+    document.getElementById('pirata').style.display = 'block';
+
+    soma();
+
+    caribe = caribe2 = caribe3 = '0';
+
+}
+
+else if (filme == ordinario || filme == ordinario2 || filme == ordinario3){
+
+    document.getElementById('extra').style.display = 'block';
+
+    soma();
+
+    ordinario = ordinario2 = ordinario3 = '0';
+
+}
+
+else if (filme == vorazes || filme == vorazes2 || filme == vorazes3){
+
+    document.getElementById('jogos').style.display = 'block';
+
+    soma();
+
+    vorazes = vorazes2 = vorazes3 = '0';
+
+}
+
+else if (filme == preto || filme == preto2 || filme == preto3){
+
+    document.getElementById('mib').style.display = 'block';
+
+    soma();
+
+    preto = preto2 = preto3 = '0';
+
+}
+
+else if (filme == manji || filme == manji2 || filme == manji3){
+
+    document.getElementById('jumanji').style.display = 'block';
+
+    soma();
+
+    manji = manji2 = manji3 = '0';
+
+}
+
+else if (filme == tix || filme == tix2 || filme == tix3){
+
+    document.getElementById('matrix').style.display = 'block';
+
+    soma();
+
+    tix = tix2 = tix3 = '0';
+}
+```
+
+}
+
+// FECHAR MENSAGEM DE ERRO
 function fechar(){
 
-    document.getElementById('alert').style.display = 'none';
+```
+document.getElementById('alert').style.display = 'none';
+
+// Devolve o foco para a caixa de resposta
+document.getElementById('resp').focus();
+```
 
 }
 
+function fechar2(){
 
-function fechar(){
-
-    document.getElementById('alert').style.display = 'none';
-
-    // Devolve o foco para a caixa de resposta
-    document.getElementById('resp').focus();
+```
+document.getElementById('instrucao').style.display = 'none';
+```
 
 }
-
 
 function mostrar(){
 
-    document.getElementById('instrucao').style.display = 'block';
+```
+document.getElementById('instrucao').style.display = 'block';
+```
 
 }
-
 
 // FINALIZAÇÃO DO JOGO
 function descricao(){
 
-    var pontos = parseInt(document.getElementById('Placar').value);
+```
+var pontos = parseInt(document.getElementById('Placar').value);
 
-    // Esconde os controles do jogo
-    document.getElementById('vai').style.display = 'none';
-    document.getElementById('Placar').style.display = 'none';
-    document.getElementById('resp').style.display = 'none';
+// Esconde os controles do jogo
+document.getElementById('vai').style.display = 'none';
+document.getElementById('Placar').style.display = 'none';
+document.getElementById('resp').style.display = 'none';
 
-    // Mostra a tela final
-    document.getElementById('pontof').style.display = 'flex';
-
-
-    // Mensagem
-    if (pontos == 1){
-
-        document.getElementById('msgf').innerHTML =
-            'Você encontrou 1 filme!';
-
-    }
-    else {
-
-        document.getElementById('msgf').innerHTML =
-            'Você encontrou ' + pontos + ' filmes!';
-
-    }
+// Mostra a tela final
+document.getElementById('pontof').style.display = 'flex';
 
 
-    // Define a página de compartilhamento
-    document.getElementById('muda').href =
-        'https://leleandrobs-lehtech.github.io/Ca-a-Referencias-Filmes/' +
-        pontos + 'pt.html';
+// Mensagem
+if (pontos == 1){
+
+    document.getElementById('msgf').innerHTML =
+        'Você encontrou 1 filme!';
+
+}
+else {
+
+    document.getElementById('msgf').innerHTML =
+        'Você encontrou ' + pontos + ' filmes!';
+
 }
 
+
+// Define a página de compartilhamento
+document.getElementById('muda').href =
+    'https://leleandrobs-lehtech.github.io/Ca-a-Referencias-Filmes/' +
+    pontos + 'pt.html';
+```
+
+}
 
 // BOTÃO NÃO
 function fecharFim(){
 
-    document.getElementById('pontof').style.display = 'none';
+```
+document.getElementById('pontof').style.display = 'none';
 
-    document.getElementById('vai').style.display = '';
-    document.getElementById('Placar').style.display = '';
-    document.getElementById('resp').style.display = '';
-    document.getElementById('mdesc').style.display = '';
+document.getElementById('vai').style.display = '';
+document.getElementById('Placar').style.display = '';
+document.getElementById('resp').style.display = '';
+document.getElementById('mdesc').style.display = '';
+```
 
 }
-
 
 // BOTÃO SIM
 function reiniciar(){
 
-    location.reload();
+```
+location.reload();
+```
 
 }
-
 
 // COMPARTILHAR NO FACEBOOK
 function compartilhar(){
 
-    var pontos = parseInt(document.getElementById('Placar').value);
+```
+var pontos = parseInt(document.getElementById('Placar').value);
 
-    // Garante que o número esteja entre 1 e 10
-    if (pontos < 1){
-        pontos = 1;
-    }
-
-    if (pontos > 10){
-        pontos = 10;
-    }
-
-
-    // Página específica para o placar
-    var url =
-        'https://leleandrobs-lehtech.github.io/Ca-a-Referencias-Filmes/' +
-        pontos + 'pt.html';
-
-
-    // Facebook
-    var facebook =
-        'https://www.facebook.com/sharer/sharer.php?u=' +
-        encodeURIComponent(url);
-
-
-    window.open(
-        facebook,
-        'facebook-share',
-        'width=600,height=500'
-    );
-
+// Garante que o número esteja entre 1 e 10
+if (pontos < 1){
+    pontos = 1;
 }
 
+if (pontos > 10){
+    pontos = 10;
+}
+
+
+// Página específica para o placar
+var url =
+    'https://leleandrobs-lehtech.github.io/Ca-a-Referencias-Filmes/' +
+    pontos + 'pt.html';
+
+
+// Facebook
+var facebook =
+    'https://www.facebook.com/sharer/sharer.php?u=' +
+    encodeURIComponent(url);
+
+
+window.open(
+    facebook,
+    'facebook-share',
+    'width=600,height=500'
+);
+```
+
+}
 
 // SAIR DO JOGO
 function sairDoJogo(){
 
-    window.location.href =
-        "COLOQUE_AQUI_O_LINK_DO_SEU_GOOGLE_SITES";
+```
+window.location.href =
+    "COLOQUE_AQUI_O_LINK_DO_SEU_GOOGLE_SITES";
+```
 
 }
 
+// ENTER FECHA A MENSAGEM DE ERRO
 document.addEventListener('keydown', function(event){
 
-    var alerta = document.getElementById('alert');
+```
+var alerta = document.getElementById('alert');
 
-    // Verifica se o alerta está aberto
-    if (alerta && alerta.style.display == 'flex'){
+// Verifica se a mensagem de erro está realmente visível
+if (alerta && getComputedStyle(alerta).display !== 'none'){
 
-        // Se apertar ENTER
-        if (event.key === 'Enter'){
+    if (event.key === 'Enter'){
 
-            event.preventDefault();
+        event.preventDefault();
 
-            fechar();
-
-        }
+        fechar();
 
     }
+
+}
+```
 
 });
